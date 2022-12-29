1c0f201ecb954a15120099107436f5d2d29b87b8
Danilo <danilo.justino1@gmail.com>
2022-12-29 02:15:50 -0300

    Fist Commit

---
+++ client/script.js
@@ -0,0 +1,112 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector("form");
+const chatContainer = document.getElementById("chat_container");
+
+let loadInterval;
+
+//Animação 3 pontinhos
+function loading(element){
+  element.textContent = '.';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if(element.textContent === '.....'){
+      element.textContent = '.';
+    }
+  }, 300)
+}
+
+//Animação Bot Escrevendo
+function typeText(element, text){
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if(index < text.length){
+      element.innerHTML += text.charAt(index);
+      index++;
+    }else{
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+//Geração de ID único por data e número aleatório Ex: "id-291220220019-1092849182"
+function generateId(){
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`; //tá em string pq é pra nome de class
+}
+
+//HTML pra gerar mensagem e imagem de perfil
+function chatStripe(isAi, message, Id) {
+  return(
+    `
+      <div class="wrapper ${isAi && 'ai'}">
+        <div class="chat">
+          <div class="profile">
+            <img src=${isAi ? bot : user} />
+          </div>
+          <div class="message" id=${Id}>${message}</div>
+        </div>
+      </div>
+    `
+  )
+}
+
+//Action do Form
+const handleSubmit = async (e) => {
+  e.preventDefault(); //não recarrega a página
+
+  const data = new FormData(form); //pega o prompt escrito
+
+  //Joga o Prompt no chat
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  form.reset();
+
+  //Ativa o Loading do Bot (FrontEnd)
+  const uniqueId = generateId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+  chatContainer.scrollTop = chatContainer.scrollHeight; //desce confome ele escreve
+  const messageDiv = document.getElementById(uniqueId);
+  loading(messageDiv);
+
+  //Request pra API do Bot
+  const response = await fetch('http://localhost:5000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+
+  //Para o Loading e Limpa o texto para ser escrito no próximo bloco
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  //Pega a resposta do backend e escreve
+  if(response.ok){
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  }else {
+    const err = await response.text();
+    messageDiv.innerHTML = "Algo deu errado.";
+    alert(err);
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
+//Aciona a função quando aperta enter
+form.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13){
+    handleSubmit(e);
+  }
+})
---
+++ server/server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+//Configuração do OpenAIApi
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+//Configuração do CORS (usado pra endpoint de API)
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//Gera uma rota de Post no Index
+app.post('/', async (req, res) => { //req = request | res = response
+    try {
+        //pega o prompt do usuário do frontend
+        const prompt = req.body.prompt;
+        
+        //gera a resposta
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0.9, //Seriedade
+            max_tokens: 4000, //Maximo de caracteres de resposta
+            top_p: 1,
+            frequency_penalty: 1, //Penalidade pra repetir frases
+            presence_penalty: 0 //Penalidade pra cada caractere de resposta
+        });
+
+        //envia a resposta de volta pro frontend
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error || 'Something went wrong');
+    }
+})
+
+//Inicia o server
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
